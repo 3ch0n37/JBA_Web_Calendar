0c07818256ddac1c1addc0e1a06614cf02f07618
Vladimir Rujević <vladimir.rujevic@live.com>
2021-07-25 12:45:31 +0200

    stage 2

--- Web Calendar/task/app.py
+++ Web Calendar/task/app.py
@@ -1,11 +1,24 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, inputs
 import sys
 
 app = Flask(__name__)
 
 # write your code here
 api = Api(app)
+eventParser = reqparse.RequestParser()
+eventParser.add_argument(
+    'event',
+    type=str,
+    help='The event name is required!',
+    required=True
+)
+eventParser.add_argument(
+    'date',
+    type=inputs.date,
+    help='The event date with the correct format is required! The correct format is YYYY-MM-DD!',
+    required=True
+)
 
 
 class WebCalendarResource(Resource):
@@ -13,7 +26,16 @@ class WebCalendarResource(Resource):
         return {'data': 'There are no events for today!'}
 
 
+class EventResource(Resource):
+    def post(self):
+        args = eventParser.parse_args()
+        if 'message' in args:
+            return {'message': args['message']}
+        return {'message': 'The event has been added!', 'event': args['event'], 'date': str(args['date'].date())}
+
+
 api.add_resource(WebCalendarResource, '/event/today')
+api.add_resource(EventResource, '/event')
 
 
 # do not change the way you run the program
